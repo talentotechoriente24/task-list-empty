b8e7a40ad92288549b1be168877e04653960ee21
Deiber Olivasres <deiber.olivares@infinitytech.dev>
2024-09-14 10:55:44 -0500

    feat: :art: added  TaskFilters component

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header'
 import TaskForm from './components/TaskForm'
 import TaskList from './components/TaskList';
 import TaskSearch from './components/TaskSearch';
+import TaskFilters from './components/TaskFilters';
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
     <>
       <Header/>
       <TaskSearch/>
+      <TaskFilters/>
       <TaskForm/>
       <TaskList tasks={tasks}/>
     </>
--- src/components/TaskFilters.jsx
+++ src/components/TaskFilters.jsx
@@ -1,8 +1,24 @@
-function TaskFilters() {
-  return (
-    <>
-    </>
-  );
-}
+import './TaskFilters.css';
 
+const TaskFilters = () => {
+  const handleOnchange = (e) => {
+    console.log('Seleccionastes: ', e.target.value)
+  }
+  return (
+    <div className="task-filters">
+      <select onChange={(e) => handleOnchange(e)}>
+        <option value="">Todas las Categorías</option>
+        <option value="Trabajo">Trabajo</option>
+        <option value="Personal">Personal</option>
+        <option value="Hogar">Hogar</option>
+      </select>
+      <select onChange={(e) => handleOnchange(e)}>
+        <option value="">Todos los estados</option>
+        <option value="completed">Completadas</option>
+        <option value="pending">Pendientes</option>
+      </select>
+    </div>
+    
+  )
+}
 export default TaskFilters;
